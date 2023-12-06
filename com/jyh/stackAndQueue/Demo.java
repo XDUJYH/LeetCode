62322f4cee975916355fbf8d08f7ebc799f2edba
JYH <xdujyh@gmail.com>
2023-12-06 22:43:06 +0800

    239

---
+++ com/jyh/stackAndQueue/Demo.java
@@ -0,0 +1,16 @@
+package com.jyh.stackAndQueue;
+
+import java.util.ArrayList;
+
+public class Demo {
+    public static void main(String[] args) {
+        Solution s1 = new Solution();
+        System.out.println(s1);
+
+    }
+}
+class Solution {
+    public void ProblemName() {
+        System.out.println("hello");
+    }
+    }
---
+++ com/jyh/stackAndQueue/LeetCode_239.java
@@ -0,0 +1,19 @@
+package com.jyh.stackAndQueue;
+
+/**
+ * 239. 滑动窗口最大值
+ * leetcode url : https://leetcode.cn/problems/sliding-window-maximum/
+ * 代码随想录 url : https://github.com/youngyangyang04/leetcode-master/blob/master/problems/0239.%E6%BB%91%E5%8A%A8%E7%AA%97%E5%8F%A3%E6%9C%80%E5%A4%A7%E5%80%BC.md
+ */
+public class LeetCode_239 {
+    public static void main(String[] args) {
+        Solution s1 = new Solution();
+        System.out.println(s1);
+
+    }
+    class Solution {
+        public int[] maxSlidingWindow(int[] nums, int k) {
+
+        }
+    }
+}
